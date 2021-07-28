c8c6a3726a5584c42799353beb710713baa032fd
Zhenya <kuprikbeer@gmail.com>
2021-07-28 23:46:47 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const res = []
+    const ed = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    const tens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    const others = ['hundred']
+    let str = '' + number
+    let arr = str.split('')
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == '0' || arr[i] == '') {
+            return 'zero'
+        }
+        switch (arr.length) {
+            case 3:
+                if (arr[i] !== '0') {
+                    res.push([ed[arr[i]]])
+                    res.push(others[0])
+                }
+                arr.shift()
+            case 2:
+                if (arr[i] == '1') {
+                    res.push(teens[arr[1]])
+                    console.log(res.join(' '))
+                    return res.join(' ')
+                } else
+                    if (arr[i] == '0') {
+                        arr.shift()
+                    } else {
+                        res.push(tens[arr[i]])
+                        arr.shift()
+                    }
+            case 1:
+                if (arr[i] != '0') {
+                    res.push(ed[arr[i]])
+                    return res.join(' ')
+                }
+        }
+        return res.join(' ')
+    }
 }
